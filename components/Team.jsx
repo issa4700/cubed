c76633407272dd5547f2e8f6062cabd3ec7922af
issa4700 <issa4700@gmail.com>
2021-09-11 23:57:17 +0100

    Fixed alt text for user images

--- components/Team.jsx
+++ components/Team.jsx
@@ -19,7 +19,7 @@ export default function Team({ team }) {
             <div key={i} className="p-2 lg:w-1/3 md:w-1/2 w-full">
               <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
                 <img
-                  alt="team"
+                  alt={member.name}
                   className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 mr-4"
                   src={`https://mc-heads.net/avatar/${member.name}/64`}
                 />
